4d4d8c1592540f9a1afc2f76a57e940ac2344e54
Nick-2805 <nikolai-pavlov-1987@mail.ru>
2022-05-26 21:36:11 +0300

    fix: change class for add task

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("new__note-title");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completedTasks");//completed-tasks
